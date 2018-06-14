4f3f3d2eff5668704572df69ce3d6fa0f7954e56
Suryamohan Sah (Digital) <SU317083@wipro.com>
2018-06-14 15:50:43 +0530

    my bugs fixed

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ const app = express();
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-const forceSSL = function() {
-  return function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
+// const forceSSL = function() {
+//   return function(req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//         ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// };
  
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+// app.use(forceSSL());
  
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
